f1ed423aab6fd3936993104f9d59bdb62c858c3e
togana <o.togana@gmail.com>
2017-03-12 16:44:58 +0900

    wip

--- src/components/todoApp.jsx
+++ src/components/todoApp.jsx
@@ -5,7 +5,18 @@ import DevTool from 'mobx-react-devtools';
 // import { ALL_TODOS, ACTIVE_TODOS, COMPLETED_TODOS } from '../constants';
 
 class TodoApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textValue: 'initial value',
+    };
+    this.changeText = this.changeText.bind(this);
+  }
   componentDidMount() {
+  }
+
+  changeText(e) {
+    this.setState({ textValue: e.target.value });
   }
 
   render() {
@@ -13,6 +24,8 @@ class TodoApp extends Component {
     return (
       <div>
         <DevTool />
+        <p>{this.state.textValue}</p>
+        <input type="text" value={this.state.textValue} onChange={this.changeText} />
       </div>
     );
   }
